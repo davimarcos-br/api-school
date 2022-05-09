03da83b7b2f2d409f33b2059a54c0b90a810351a
davimarcos-br <braginha@gmail.com>
2022-05-09 11:48:00 -0300

    add src

--- src/app.js
+++ src/app.js
@@ -4,7 +4,11 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res)=>{
+    res.send('rodando')
+})
 
 app.listen(3000,()=>{
     console.log('rodando');
+    console.log('http://localhost:3000/');
 })
