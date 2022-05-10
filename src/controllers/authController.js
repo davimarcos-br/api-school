ece51a11f37784a7d7852430665ad01188e73819
davimarcos-br <braginha@gmail.com>
2022-05-10 17:17:12 -0300

    atualização

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,22 +1,41 @@
 const express = require('express')
 const User = require('../models/users')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const router = express.Router()
 
 
 router.post('/register', async ( req, res)=>{
+    const  { email } = req.body
     try {
+        if (await User.findOne({ email })) 
+            return res.status(400).send({ error: "Usuario já cadastrado" })
         const user = await User.create(req.body)
         
-        console.log(req.body)
-        return res.send(user)
+        console.log({user})
+        user.password = undefined
+        return res.send({user})
     } catch (err) {
         return res.status(400).send(err)
     }
 })
 
-router.get('/register', (req, res)=>{
-    res.send('teste')
+router.post('/autheticate', async (req, res)=>{
+    const { email , password } = req.body
+
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user)
+        return res.status(400).send({ error: 'Usuario não cadastrado' })
+
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'Senha inválida' })
+    user.password = undefined
+
+    const token = jwt.sign({ id: user._id })
+    res.send({ user })
+
 })
 
 module.exports = app => app.use('/auth', router)
